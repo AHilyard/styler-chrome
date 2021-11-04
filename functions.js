41e12d313c3eab46562eef2d889937facd14cb91
Anthony Hilyard <anthony.hilyard@gmail.com>
2021-11-04 10:12:02 -0700

    Rules now apply to base domains, removed purple button.

--- functions.js
+++ functions.js
@@ -17,22 +17,30 @@ function AP(data) {
 }
 
 function get_hostname(url) {
-    var hostname;
-    //find & remove protocol (http, ftp, etc.) and get hostname
+	var hostname;
 
-    if (url.indexOf("://") > -1) {
-        hostname = url.split('/')[2];
-    }
-    else {
-        hostname = url.split('/')[0];
-    }
+	//find & remove protocol (http, ftp, etc.) and get hostname
+	if (url.indexOf("://") > -1)
+	{
+		hostname = url.split('/')[2];
+	}
+	else
+	{
+		hostname = url.split('/')[0];
+	}
 
-    //find & remove port number
-    hostname = hostname.split(':')[0];
-    //find & remove "?"
-    hostname = hostname.split('?')[0];
+	//find & remove port number
+	hostname = hostname.split(':')[0];
+	//find & remove "?"
+	hostname = hostname.split('?')[0];
 
-    return hostname;
+	let hostnames = hostname.split('.');
+	if (hostnames.length > 1)
+	{
+		hostname = hostnames[hostnames.length - 2] + '.' + hostnames[hostnames.length - 1];
+	}
+
+	return hostname;
 }
 
 function save_settings(hostname, settings) {
